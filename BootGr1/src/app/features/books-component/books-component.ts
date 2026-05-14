862bf504d0f9e9cab0a11510a626c3db94d921db
Freddy Barahona <shadsdemon@gmail.com>
2026-05-14 12:04:17 -0500

    feat: generacion del componente libros

---
+++ BootGr1/src/app/features/books-component/books-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-component',
+  imports: [],
+  templateUrl: './books-component.html',
+  styleUrl: './books-component.scss',
+})
+export class BooksComponent {
+
+}
