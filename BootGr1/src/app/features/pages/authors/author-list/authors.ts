05bd20f6293254712843aa82c8a71ece5ca2237d
ElCabaKing <davi.cabanilla@gmail.com>
2026-05-14 12:18:52 -0500

    feat: implement confirmation dialog for author deletion and update styles

--- BootGr1/src/app/features/pages/authors/author-list/authors.ts
+++ BootGr1/src/app/features/pages/authors/author-list/authors.ts
@@ -13,7 +13,7 @@ import { ConfirmElimination } from '../confirm-elimination/confirm-elimination';
 
 export class Authors implements OnInit {
   private readonly authorService = inject(AuthorsService);
-  public authorList= signal<IAuthor[]>([]);
+  public authorList = signal<IAuthor[]>([]);
 
   ngOnInit() {
     this.authorService.getAll().subscribe((authors) => {
@@ -22,10 +22,21 @@ export class Authors implements OnInit {
   }
 
   dialog = inject(MatDialog);
-  
+
   confirmDelete() {
     const dialogRef = this.dialog.open(ConfirmElimination, {
       data: null
     });
-  }
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result?.delete) {
+        this.authorService.delete('id').subscribe(() => {
+          // Aquí puedes actualizar la lista de autores después de eliminar uno
+          this.authorService.getAll().subscribe((authors) => {
+            this.authorList.set(authors);
+          });
+        });
+      }
+    });
+}
 }
