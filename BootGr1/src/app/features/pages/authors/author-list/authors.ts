fb9449120e0f5430553cbf5821fb69f4e6b4d990
Neider Vélez <velezzambranoneider@gmail.com>
2026-05-14 12:11:20 -0500

    feat: agregar funcionalidad al boton añadir autor

--- BootGr1/src/app/features/pages/authors/author-list/authors.ts
+++ BootGr1/src/app/features/pages/authors/author-list/authors.ts
@@ -1,6 +1,8 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { AuthorsService } from '../../../services/authors-service';
 import { IAuthor } from '../../../interfaces/IAuthor';
+import { AuthorForm } from '../author-form/author-form';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-authors',
@@ -12,9 +14,19 @@ export class Authors implements OnInit {
   private readonly authorService = inject(AuthorsService);
   public authorList= signal<IAuthor[]>([]);
 
+  constructor(private dialog: MatDialog) {}
+
   ngOnInit() {
     this.authorService.getAll().subscribe((authors) => {
       this.authorList.set(authors);
     });
   }
+
+  abrirFormulario(author: IAuthor | null = null){
+    const dialogRef = this.dialog.open(AuthorForm, {
+      width: '480px',
+      data: author
+    })
+  }
+
 }
