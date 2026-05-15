e0cddcf1218f7a03c17a1cb257d22be95d0bf003
jeffersonveloz-lab <jeffersonvelozh@gmail.com>
2026-05-15 15:12:31 -0500

    agaregando las finciones en los archivos booklis.ts y bookform.ts

--- BootGr1/src/app/features/pages/books/book-form/book-form.ts
+++ BootGr1/src/app/features/pages/books/book-form/book-form.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { IBook } from '../../../interfaces/IBook';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BooksService } from '../../../services/books-service/books-service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-form',
@@ -6,4 +11,46 @@ import { Component } from '@angular/core';
   templateUrl: './book-form.html',
   styleUrl: './book-form.scss',
 })
-export class BookForm {}
+export class BookForm {
+  private bookService = inject(BooksService);
+
+  loading = signal(false);
+  errorMessage = signal('');
+  isEdit = signal<boolean | null>(null);
+  book: IBook | null = inject(MAT_DIALOG_DATA)
+
+  constructor(private router: Router){}
+
+  form = new FormGroup({
+    name: new FormControl('', {nonNullable: true,
+      validators: [Validators.required,
+        Validators.minLength(3)]
+    }),
+    author: new FormControl('', {nonNullable: true,
+      validators: [Validators.required,
+        Validators.minLength(3)]
+    }),
+    publisheAt: new FormControl('', {nonNullable: true,
+      validators: [Validators.required]
+    })
+  });
+
+  guardar() {
+    if (this.form.invalid) return;
+
+    const libro: Partial<IBook> =
+      this.form.getRawValue();
+
+  const accion = this.isEdit()
+    ? this.bookService.UpdateBook(this.book!.id, libro)
+    : this.bookService.AddBook(libro);
+
+    accion.subscribe({
+      next: () => this.router.navigate(['/books'])
+    });
+  }
+
+  cerrar(): void {
+    this.router.navigate(['/books']);
+  }
+}
--- BootGr1/src/app/features/pages/books/book-list/book-list.ts
+++ BootGr1/src/app/features/pages/books/book-list/book-list.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { IBook } from '../../../interfaces/IBook';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { BooksService } from '../../../services/books-service/books-service';
+import { Router } from '@angular/router';
+import { BookForm } from '../book-form/book-form';
+
 
 @Component({
   selector: 'app-book-list',
@@ -6,4 +12,50 @@ import { Component } from '@angular/core';
   templateUrl: './book-list.html',
   styleUrl: './book-list.scss',
 })
-export class BookList {}
+export class BookList {
+
+  books = signal<IBook[]>([]);
+  loading = signal(false);
+  erroMessage = signal('');
+
+  constructor( private bookService: BooksService, private dialogRef: MatDialog) {}
+
+  private router = inject(Router);
+
+
+  ngOnInit(): void {
+    this.cargarLibros();
+  }
+
+
+  cargarLibros(): void {
+
+    this.loading.set(true);
+
+    this.bookService.getAllBooks().subscribe({
+
+      next: (data: IBook[]) => {
+        this.books.set(data);
+        this.loading.set(false);
+      },
+
+      error: () => {
+        this.erroMessage.set('Error al cargar los libros');
+        this.loading.set(false);
+      }
+
+    });
+
+  }
+
+    goToBookDetails(Id: string): void {
+    this.router.navigate(['/books', Id]);
+  }
+
+  abrirFormulario(book: IBook | null = null){
+    const dialogRef = this.dialogRef.open(BookForm, {
+      width: '480px',
+      data: book
+    })
+  }
+  }
